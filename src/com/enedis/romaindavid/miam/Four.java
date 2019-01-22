da6fa6770711139c317b7458710da86d873cab58
RomainDavidS <romaindavid.sergeant@gmail.com>
2019-01-22 17:05:35 +0100

    Mise à jour des class de miam

--- src/com/enedis/romaindavid/miam/Four.java
+++ src/com/enedis/romaindavid/miam/Four.java
@@ -1,7 +1,19 @@
 package com.enedis.romaindavid.miam;
 
-/**
- *
- */
+
 public class Four {
+    int puissance;
+    int capacite;
+
+    /**
+     *
+     */
+
+    public void cuire(Aliment aliment) {
+        System.out.println("Je cuis un aliment : " + aliment.nom);
+        System.out.println("avec ma capacité de " + capacite + " litres");
+        System.out.println("et ma puissance de " + puissance + " degrés.");
+        aliment.estCuit = true;
+    }
+
 }
--- src/com/enedis/romaindavid/miam/Main.java
+++ src/com/enedis/romaindavid/miam/Main.java
@@ -2,16 +2,17 @@ package com.enedis.romaindavid.miam;
 
 public class Main {
     public static void main(String[] args) {
-        Four petitFour = new Four();
-        petitFour.capacite = 30;
-        petitFour.puissance = 180;
-
         Four grandFour = new Four();
         grandFour.capacite = 55;
         grandFour.puissance = 260;
 
-        petitFour.cuire();
+        Aliment cake = new Aliment();
+        cake.nom = "cake aux fruits";
+        cake.estCuit = false;
+        cake.manger();
         System.out.println("");
-        grandFour.cuire();
+        grandFour.cuire(cake);
+        System.out.println("");
+        cake.manger();
     }
 }
