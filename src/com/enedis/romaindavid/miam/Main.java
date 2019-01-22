953fb694cbf1490817746ff69a65ced6276dc111
RomainDavidS <romaindavid.sergeant@gmail.com>
2019-01-22 16:54:12 +0100

    Modification des class de miam

--- src/com/enedis/romaindavid/miam/Main.java
+++ src/com/enedis/romaindavid/miam/Main.java
@@ -2,6 +2,16 @@ package com.enedis.romaindavid.miam;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Four petitFour = new Four();
+        petitFour.capacite = 30;
+        petitFour.puissance = 180;
+
+        Four grandFour = new Four();
+        grandFour.capacite = 55;
+        grandFour.puissance = 260;
+
+        petitFour.cuire();
+        System.out.println("");
+        grandFour.cuire();
     }
 }
